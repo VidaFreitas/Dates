48b558babca3de93c96b129d63918cb62ef18de7
Vida Valessa <vidavalessa65@gmail.com>
2021-01-07 17:47:13 -0300

    Starting with dates

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // var data = DateTime();
+            var data = DateTime.Now;
+            Console.WriteLine(data);
+        }
+    }
+}
