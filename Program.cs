95348045e72b5891e3cedd7d3b93af3578134a8b
Vida Valessa <vidavalessa65@gmail.com>
2021-01-07 17:53:46 -0300

    Getting date values

--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ namespace Dates
         {
             Console.Clear();
 
-            // var data = DateTime();
-            var data = DateTime.Now;
+            var data = new DateTime(2021, 01, 07, 17, 50, 12);
+            // var data = DateTime.Now;
             Console.WriteLine(data);
+            Console.WriteLine(data.Year);
+            Console.WriteLine(data.Month);
+            Console.WriteLine(data.Day);
         }
     }
 }
